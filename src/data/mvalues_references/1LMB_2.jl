4f9416cc8d4afcb8134d2e4764c886a7c617c72c
ana-bblima <ana.blima@protonmail.com>
2025-11-30 15:52:09 -0300

    9RSA and 1LMB_2 FIXES: pdb files, bolen server output -sasa and mvalue -

--- src/data/mvalues_references/1LMB_2.jl
+++ src/data/mvalues_references/1LMB_2.jl
@@ -11,18 +11,18 @@ mvalues_moeser_horinek["1LMB_2"] = OrderedDict(
 )
 
 mvalues_auton_bolen["1LMB_2"] = parse_mvalue_server("""
-Native and Denatured State Transfer Free Energy Contributions to the m-value for /tmp/phpVUggEV. 
+Native and Denatured State Transfer Free Energy Contributions to the m-value for /tmp/phpiiexG4. 
 		    Native State (cal/mol/M) 							 Denatured State (cal/mol/M) 
 Osmolyte 	 Backbone   Sidechains    Total 	 | 		 Backbone 			 Sidechains 			 Total 
-1M TMAO             579        -439         140 	 | 	 (   268)    356 [   443] 	 (  -380)   -402 [  -423] 	 (  -112)    -46 [    20] 
-1M Sarcosine        335        -134         200 	 | 	 (   155)    206 [   256] 	 (  -113)   -120 [  -128] 	 (    42)     85 [   129] 
-1M Betaine          431        -594        -163 	 | 	 (   200)    265 [   330] 	 (  -516)   -546 [  -576] 	 (  -317)   -281 [  -246] 
-1M Proline          309        -260          49 	 | 	 (   143)    190 [   237] 	 (  -221)   -234 [  -248] 	 (   -77)    -44 [   -11] 
-1M Glycerol          90        -118         -28 	 | 	 (    42)     55 [    69] 	 (  -107)   -112 [  -117] 	 (   -65)    -56 [   -48] 
-1M Sorbitol         225         -76         149 	 | 	 (   104)    138 [   172] 	 (   -70)    -73 [   -77] 	 (    35)     65 [    95] 
-1M Sucrose          399        -149         250 	 | 	 (   185)    245 [   305] 	 (  -133)   -140 [  -146] 	 (    52)    105 [   159] 
-1M Trehalose        399        -188         211 	 | 	 (   185)    245 [   305] 	 (  -169)   -177 [  -185] 	 (    16)     69 [   121] 
-1M Urea            -251         102        -149 	 | 	 (  -116)   -154 [  -192] 	 (    83)     89 [    94] 	 (   -33)    -66 [   -98] 
+1M TMAO            2364       -1804         560 	 | 	 (  3891)   5386 [  6881] 	 ( -2310)  -2511 [ -2713] 	 (  1581)   2875 [  4169] 
+1M Sarcosine       1366        -512         854 	 | 	 (  2248)   3112 [  3976] 	 (  -288)   -281 [  -275] 	 (  1960)   2831 [  3701] 
+1M Betaine         1760       -2647        -887 	 | 	 (  2897)   4010 [  5123] 	 ( -3652)  -3946 [ -4241] 	 (  -756)     64 [   882] 
+1M Proline         1261       -1692        -431 	 | 	 (  2075)   2873 [  3670] 	 ( -2259)  -2456 [ -2655] 	 (  -183)    416 [  1015] 
+1M Glycerol         368        -892        -524 	 | 	 (   605)    838 [  1070] 	 ( -1161)  -1242 [ -1323] 	 (  -556)   -405 [  -253] 
+1M Sorbitol         919        -726         193 	 | 	 (  1513)   2095 [  2676] 	 (  -529)   -546 [  -563] 	 (   984)   1548 [  2113] 
+1M Sucrose         1628        -876         752 	 | 	 (  2680)   3710 [  4741] 	 (  -862)   -898 [  -936] 	 (  1819)   2812 [  3805] 
+1M Trehalose       1628        -765         864 	 | 	 (  2680)   3710 [  4741] 	 (   -12)     61 [   135] 	 (  2668)   3772 [  4875] 
+1M Urea           -1024         519        -506 	 | 	 ( -1686)  -2334 [ -2982] 	 (   545)    601 [   655] 	 ( -1141)  -1733 [ -2327] 
 """)
 
 mvalues_experimental["1LMB_2"] = OrderedDict(
