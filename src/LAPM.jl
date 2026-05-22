e14ac1d68a8b53561d023b40441969c863dc1734
Leandro Martinez <lmartine@unicamp.br>
2026-05-22 15:18:42 -0300

    adjust legends of exeprimental plot

--- src/LAPM.jl
+++ src/LAPM.jl
@@ -289,34 +289,56 @@ function plot_MH_vs_AB(
     return plt
 end
 
-function plot_experimental(
+plot_experimental(
     model::Type{<:PDBTools.MValueModel}=MoeserHorinek,
     cosolvent="urea";
     sasas_from::Function=creamer_sasa
+) = plot_experimental([model], cosolvent; sasas_from)
+
+function plot_experimental(
+    models=[AutonBolen, MoeserHorinek, MoeserHorinekFit],
+    cosolvent="urea";
+    sasas_from::Function=creamer_sasa
 )
+    plt = plot(layout=(1,length(models)))
     cosolvent = lowercase(cosolvent)
     cosolvent_exp = cosolvent == "urea-app" ? "urea" : cosolvent
     example_structs = keys(sasa_server)
     nexamples = length(example_structs)
-    tot_pred = zeros(nexamples)
-    tot_exp = zeros(nexamples)
-    for (i, str) in enumerate(example_structs)
-        p = predict_mvalue(str; cosolvent, model, sasas_from)
-        tot_pred[i] = p.tot
-        tot_exp[i] = mvalues_experimental[str][cosolvent_exp]
+    sp=0
+    for model in models
+        sp += 1
+        tot_pred = zeros(nexamples)
+        tot_exp = zeros(nexamples)
+        for (i, str) in enumerate(example_structs)
+            p = predict_mvalue(str; cosolvent, model, sasas_from)
+            tot_pred[i] = p.tot
+            tot_exp[i] = mvalues_experimental[str][cosolvent_exp]
+        end
+        plot!(plt,framestyle=:box, fontfamily="Computer Modern", subplot=sp)
+        ls = (lw=2, ls=:dash, label="", lc=:lightgrey)
+        plot!(plt, [-100, 100], [-100, 100]; subplot=sp, ls...)
+        _scatter!(plt, tot_exp, tot_pred, example_structs; legend_title="", subplot=sp)
+        plot!(plt, xlabel=L"\textrm{Experimental~/~kcal~mol^{-1}}", subplot=sp)
+        plot!(plt, ylabel=modelname(model), subplot=sp)
+        fit = fitlinear(tot_exp, tot_pred)
+        plot!(plt,
+            subplot=sp,
+            legend_title=latexstring(
+                "a="*string(round(fit.a; digits=3))*";~"*
+                "b="*string(round(fit.b; digits=3))*";~"*
+                "R^2="*string(round(fit.R2; digits=3))
+            ), 
+            legend=:topleft
+        )
+        plot!(plt,xlims=(-3.5,0.2), ylims=(-3.5,0.2), subplot=sp)
     end
-    plt = plot(framestyle=:box, fontfamily="Computer Modern")
-    ls = (lw=2, ls=:dash, label="", lc=:lightgrey)
-    plot!(plt, [-100, 100], [-100, 100]; ls...)
-    _scatter!(plt, tot_exp, tot_pred, example_structs; legend_title="", subplot=1)
-    plot!(plt, xlabel=L"\textrm{Experimental~/~kcal~mol^{-1}}")
-    plot!(plt, ylabel=modelname(model))
-    plot!(plt, size=(400,400))
-    fit = fitlinear(tot_exp, tot_pred)
-    plot!(legend_title="a=$(round(fit.a; digits=3))\nb=$(round(fit.b; digits=3))\nR2=$(round(fit.R2; digits=3))", legend=:topleft)
-    plot!(xlims=(-3.5,0.2), ylims=(-3.5,0.2))
+    plot!(plt, 
+        size=(length(models) * 400,400),
+        leftmargin=0.6Plots.Measures.cm,
+        bottomargin=0.5Plots.Measures.cm,
+    )
     return plt
-#    return fit
 end
 
 include("./per_atom_type/get_sasa_per_type.jl")
